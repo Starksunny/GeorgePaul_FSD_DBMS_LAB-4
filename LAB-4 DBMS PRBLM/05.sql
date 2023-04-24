942ff306327e122618c417b3ea19dc583f552f38
Starksunny <93263093+Starksunny@users.noreply.github.com>
2023-04-25 00:41:15 +0530

    Update 05.sql

--- LAB-4 DBMS PRBLM/05.sql
+++ LAB-4 DBMS PRBLM/05.sql
@@ -1,3 +1,10 @@
 -- Q5) Display the Supplier details of who is supplying more than one product.
 
-select supp_id, count(supp_id) from supplier_pricing group by supp_id;
+select sup.supp_id, supp_name, supp_city, supp_phone 
+from supplier as sup
+inner join
+(
+select * from supplier_pricing group by 
+supp_id having count(supp_id) > 1 
+) as sp
+on sup.supp_id = sp.supp_id;
